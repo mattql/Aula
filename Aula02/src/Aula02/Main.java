7ced76c49a19f7db430fc6b9d47bde342eafe640
mattql <58866633+mattql@users.noreply.github.com>
2021-08-14 22:05:14 -0300

    Add files via upload

---
+++ Aula02/src/Aula02/Main.java
@@ -0,0 +1,9 @@
+package Aula02;
+public class Main {
+	public static void main(String[] args) {
+        Caneta c1 = new Caneta("Nic", "Amarelo", 1.5f);
+        c1.status();
+        Caneta c2 = new Caneta("Lau", "Verde", 2.0f);
+        c2.status();
+	}
+}
